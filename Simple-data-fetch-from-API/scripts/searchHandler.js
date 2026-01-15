0fa5cab454eb99da9d878a343af07aecef96a28c
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-01-15 17:48:43 +0200

    feat: localStorage

--- Simple-data-fetch-from-API/scripts/searchHandler.js
+++ Simple-data-fetch-from-API/scripts/searchHandler.js
@@ -1,6 +1,15 @@
 import { fetchCountryData } from './api.js';
 import { createCountryCard, renderToElement } from './ui.js';
 
+function saveToLocalStorage(countryName) {
+    let searches = JSON.parse(localStorage.getItem('searches')) || [];
+
+    if (!searches.includes(countryName)) {
+        searches.push(countryName);
+        localStorage.setItem('searches', JSON.stringify(searches));
+    }
+}
+
 export async function handleSearch(input, output, message) {
     const countryName = input.value.trim();
     
@@ -14,6 +23,8 @@ export async function handleSearch(input, output, message) {
 
     try {
         const country = await fetchCountryData(countryName);
+        console.log(country);
+        saveToLocalStorage(country.name.official);
         const cardHtml = createCountryCard(country);
         renderToElement(output, cardHtml);
     } catch (error) {
